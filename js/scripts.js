9dcb0fd70819e64ceba00e9e92dda497e1096b41
Federico "Morg" Pareschi <morgawr@gmail.com>
2021-02-14 17:07:21 +0900

    Add base upload behaviour

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+var file;
+
+function previewImage(file) {
+  var previewId = "preview";
+  var preview = document.getElementById(previewId);
+  var imageType = /image.*/;
+  if (!file.type.match(imageType)) {
+    console.log("Cannot display non-image type.");
+    return;
+  }
+  var thumb = document.createElement("div");
+  thumb.classList.add('thumbnail'); // Add the class thumbnail to the created div
+  var img = document.createElement("img");
+  img.file = file;
+  thumb.appendChild(img);
+  preview.appendChild(thumb);
+  // Using FileReader to display the image content
+  var reader = new FileReader();
+  reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
+  reader.readAsDataURL(file);
+}
+
+function doUpload() {
+  var url = 'http://localhost:8080/upload'
+  var xhr = new XMLHttpRequest();
+  var fd = new FormData();
+  xhr.open("POST", url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      // Every thing ok, file uploaded
+      console.log(xhr.responseText); // handle response.
+    }
+  };
+  fd.append("file", file);
+  fd.append("password", document.getElementById('password').text);
+  fd.append("duration", 60);
+  xhr.send(fd);
+}
+
+window.onload = function() {
+  document.getElementById('fileinput').addEventListener('change', function(){
+    file = this.files[0];
+    // This code is only for demo ...
+    console.log("name : " + file.name);
+    console.log("size : " + file.size);
+    console.log("type : " + file.type);
+    console.log("date : " + file.lastModified);
+    previewImage(file);
+  }, false);
+  document.getElementById('upload').addEventListener('click', function(){
+    if(file != null) {
+      doUpload();
+    }
+  });
+
+}
